dacba68376ce7cc6b17f793520a858fae282842d
Harish <harishbalkhande@gmail.com>
2021-04-18 09:55:19 +0530

    Reset password(forget password) and change password are done.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField, PasswordChangeForm,PasswordResetForm,SetPasswordForm
 from django.contrib.auth import password_validation
 from django.utils.translation import gettext,gettext_lazy as _
 from django.contrib.auth.models import User
@@ -20,6 +20,14 @@ class LoginForm(AuthenticationForm):
 class MyPwdChangeForm(PasswordChangeForm):
     old_password = forms.CharField(label=_("Old Password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'current-password','autofocus':True,'class':'form-control'}))
     
+    new_password1 = forms.CharField(label=_("New Password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','autofocus':True,'class':'form-control'}), help_text=password_validation.password_validators_help_text_html())
+    
+    new_password2 = forms.CharField(label=_("Confirm New Password"), strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','autofocus':True,'class':'form-control'}))
+
+class MyPwdResetForm(PasswordResetForm):
+    email = forms.EmailField(label=_('Email'), max_length=254, widget=forms.EmailInput(attrs={'autocomplete':'email','class':'form-control'}))
+
+class MySetPwdForm(SetPasswordForm):
     new_password1 = forms.CharField(label=_("New Password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','autofocus':True,'class':'form-control'}), help_text=password_validation.password_validators_help_text_html())
     
     new_password2 = forms.CharField(label=_("Confirm New Password"), strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','autofocus':True,'class':'form-control'}))
